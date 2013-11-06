673e6028a08ea1608e35bc9aa04cf9c0c1e2cefb
BigGreenBudz <biggreenbudz@live.com>
2013-11-06 01:29:08 -0500

    V1.0
    type /always-day to enable/disable.

---
+++ AlwaysDay/AlwaysDay.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+
+using Terraria;
+using TerrariaApi;
+using TerrariaApi.Server;
+
+using TShockAPI;
+
+namespace AlwaysDay
+{
+
+    [ApiVersion(1, 14)]
+    public class AlwaysDay : TerrariaPlugin
+    {
+
+
+
+
+        private bool AlwaysDayToggle = false;
+        private DateTime LastCheck = DateTime.UtcNow;
+        private DateTime OtherLastCheck = DateTime.UtcNow;
+        
+        
+        
+        
+        
+        public override Version Version
+        {
+            get { return new Version("1.0.0.0"); }
+        }
+
+        public override string Name
+        {
+            get { return "Always Day"; }
+        }
+
+        public override string Author
+        {
+            get { return "BigGreenBudz"; }
+        }
+
+        public override string Description
+        {
+            get { return "Allows for the time to be set to day and stay that way."; }
+        }
+
+        public AlwaysDay(Main game)
+            : base(game)
+        {
+            Order = -4;
+        }
+
+        #region Initialize
+        public override void Initialize()
+        {
+            var Hook = ServerApi.Hooks;
+
+            Hook.GameUpdate.Register(this, OnUpdate);
+
+            Commands.ChatCommands.Add(new Command("TShockAPI.Permissions.alwaysdaytoggle", AlwaysDay1, "always-day"));
+            
+        }
+#endregion
+
+        #region Dispose
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                var Hook = ServerApi.Hooks;
+
+                Hook.GameUpdate.Deregister(this, OnUpdate);
+            }
+            base.Dispose(disposing);
+        }
+#endregion
+
+         #region OnUpdate
+         public void OnUpdate(EventArgs args)
+                {
+                    if (AlwaysDayToggle == true)
+                    {
+                        TSPlayer.Server.SetTime(true, 27000.0);
+                    }
+
+                }
+#endregion
+
+#region AlwaysDayToggle 
+        public void AlwaysDay1(CommandArgs args)
+                     {
+            if (args.Player != null)
+            {
+                AlwaysDayToggle = !AlwaysDayToggle;
+                if (AlwaysDayToggle == true)
+                {
+                    
+                   
+                }
+                args.Player.SendInfoMessage("Always Day now: " + ((AlwaysDayToggle) ? "Enabled" : "Disabled"));
+            }
+                     }
+#endregion
+    }
+}
